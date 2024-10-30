5664491b2c84149ded4aa3620f590806dc0f8b84
hugojavid5 <hugojavidferrero@gmail.com>
2024-10-30 19:24:52 +0100

    Clase con javadocs

--- src/main/java/org/hugo/ejercicioo/ProductoLabel.java
+++ src/main/java/org/hugo/ejercicioo/ProductoLabel.java
@@ -1,4 +1,5 @@
 package org.hugo.ejercicioo;
+
 import java.io.IOException;
 
 import javafx.fxml.FXML;
@@ -8,21 +9,27 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
-
+/**
+ * Clase que representa un etiqueta de producto, que se compone de un nombre,
+ * un stock y una imagen. Extiende de {@link GridPane} para facilitar el diseño.
+ */
 public class ProductoLabel extends GridPane {
 
+    /** Etiqueta para mostrar el nombre del producto. */
     @FXML
     protected Label lbl_Nombre;
 
+    /** Etiqueta para mostrar la cantidad en stock del producto. */
     @FXML
     protected Label lbl_Stock;
 
-    /** The img producto. */
+    /** Imagen del producto. */
     @FXML
     private ImageView img_Producto;
 
     /**
-     * Instantiates a new producto label.
+     * Crea una nueva instancia de {@code ProductoLabel}.
+     * Carga el archivo FXML correspondiente para definir la interfaz gráfica.
      */
     public ProductoLabel() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("EjercicioO.fxml"));
@@ -35,25 +42,40 @@ public class ProductoLabel extends GridPane {
         }
     }
 
-
+    /**
+     * Establece la cantidad de stock del producto y aplica un estilo
+     * visual basado en la cantidad.
+     *
+     * @param cantidad La cantidad de stock del producto.
+     */
     public void setStock(double cantidad) {
         String style;
         if (cantidad > 100) {
-            style = "verde";
+            style = "verde"; // Estilo para stock alto
         } else if (cantidad > 20) {
-            style = "azul";
+            style = "azul"; // Estilo para stock medio
         } else {
-            style = "rojo";
+            style = "rojo"; // Estilo para stock bajo
         }
         lbl_Stock.setText(cantidad + "");
         this.getStyleClass().clear();
         this.getStyleClass().add(style);
     }
 
+    /**
+     * Establece la imagen del producto.
+     *
+     * @param imagen La imagen a establecer para el producto.
+     */
     public void setImage(Image imagen) {
         img_Producto.setImage(imagen);
     }
 
+    /**
+     * Establece el nombre del producto.
+     *
+     * @param nombre El nombre a establecer para el producto.
+     */
     public void setName(String nombre) {
         lbl_Nombre.setText(nombre + "");
     }
